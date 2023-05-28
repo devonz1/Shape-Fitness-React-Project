78920889b28305e92978ba48938c12d148ae8036
devon gibson <devongibs2@gmail.com>
2023-05-27 22:15:57 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 
 import React, { useState } from 'react';
-import axios from 'axios';
+ 
 import Create from "./create";
 import shapef from "C:/Users/devon/Promineo/shape-fitness-react-project/src/shapef.png";
 import './App.css';
